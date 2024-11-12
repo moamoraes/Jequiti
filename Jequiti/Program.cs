1edf83966fd7b1f66b9e5b5eb4caf1bf0b089095
Moacir Moraes <42810516+moamoraes@users.noreply.github.com>
2024-11-12 18:09:33 -0300

    Commit inicial

---
+++ Jequiti/Program.cs
@@ -0,0 +1,60 @@
+using Timer = System.Windows.Forms.Timer;
+namespace Jequiti
+{
+    internal static class Program
+    {
+
+        [STAThread]
+        private static async Task Main()
+        {
+            string imagePath;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            string fileName = "jequiti.jpg";
+
+            imagePath = Path.Combine(currentDirectory, fileName);
+
+            while (true)
+            {
+                ShowImageFullscreen(imagePath);
+
+                await Task.Delay(TimeSpan.FromMinutes(10)); // Exibe a cada 10min
+            }
+        }
+
+        static void ShowImageFullscreen(string imagePath)
+        {
+            Form imageForm = new Form
+            {
+                FormBorderStyle = FormBorderStyle.None,
+                WindowState = FormWindowState.Maximized,
+                BackColor = Color.Black,
+                TopMost = true
+            };
+
+            PictureBox pictureBox = new PictureBox
+            {
+                Image = Image.FromFile(imagePath),
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Dock = DockStyle.Fill
+            };
+            imageForm.Controls.Add(pictureBox);
+
+            Timer closeTimer = new Timer { Interval = 250 }; // Exibe por 0,25s
+            closeTimer.Tick += (sender, e) =>
+            {
+                closeTimer.Stop();
+                imageForm.Close();
+            };
+
+            closeTimer.Start();
+
+            imageForm.ShowDialog();
+        }
+    }
+
+}
